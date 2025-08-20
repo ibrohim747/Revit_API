5d54d70d6eab02b0e48ca92af862f64d846b9484
ibrohim747 <ibrohim7477454@gmail.com>
2025-08-20 21:35:52 +0300

    Add 27 and 28

---
+++ Pr_027.cs
@@ -0,0 +1,53 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.Creation;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using Document = Autodesk.Revit.DB.Document;
+
+namespace Projects
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    internal class Pr_027 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            using (Transaction tx = new Transaction(doc, "Create Viev plan"))
+            {
+                tx.Start();
+
+                ViewFamilyType floorPlanViewFamilyType = new FilteredElementCollector(doc).OfClass(typeof(ViewFamilyType)).Cast<ViewFamilyType>().FirstOrDefault(vft => vft.ViewFamily == ViewFamily.FloorPlan);
+
+                Reference pickedRef = uidoc.Selection.PickObject(ObjectType.Element, "Select a Level");
+                Element elem = doc.GetElement(pickedRef);
+                Level level = elem as Level;
+
+                if (level == null)
+                {
+                    TaskDialog.Show("Ошибка", "Выбранный элемент не является уровнем.");
+                    return Result.Cancelled;
+                }
+
+                ViewPlan newFloorPlan = ViewPlan.Create(doc, floorPlanViewFamilyType.Id, level.Id);
+
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Pr_028.cs
@@ -0,0 +1,59 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.Creation;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+using System.Xml;
+using System.Xml.Linq;
+using Document = Autodesk.Revit.DB.Document;
+
+namespace Projects
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    internal class Pr_028 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // Выбираем элементы (например, стены)
+            Autodesk.Revit.DB.Reference ref1 = uidoc.Selection.PickObject(ObjectType.Element, "Выберите первый элемент");
+            Autodesk.Revit.DB.Reference ref2 = uidoc.Selection.PickObject(ObjectType.Element, "Выберите второй элемент");
+
+            // Получаем активный вид (должен быть план или разрез, НЕ 3D)
+            View view = doc.ActiveView;
+
+            // Создаём линию размера (ось, на которой будет размер)
+            XYZ p1 = new XYZ(0, 0, 0);
+            XYZ p2 = new XYZ(5, 0, 0);
+            Line dimLine = Line.CreateBound(p1, p2);
+
+            // Собираем References для размера
+            ReferenceArray refArray = new ReferenceArray();
+            refArray.Append(ref1);
+            refArray.Append(ref2);
+
+            using (Transaction tx = new Transaction(doc, "Create Dimension"))
+            {
+                tx.Start();
+
+                // Создание размера
+                Dimension dim = doc.Create.NewDimension(view, dimLine, refArray);
+
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
